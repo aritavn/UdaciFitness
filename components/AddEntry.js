a485c3ab4fc75d18dca73e41d59f564dc8e84867
aritavn <anaritavn@gmail.com>
2020-06-06 21:30:43 +0100

    Update UdaciSlider and UdaciStepper and add TextButton component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
 class AddEntry extends Component {
 
@@ -43,8 +45,36 @@ class AddEntry extends Component {
     }))
   }
 
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0
+    }))
+  }
+
+  reset = () => {
+    const key = timeToString()
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons name='md-happy' size={100} />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
@@ -71,9 +101,18 @@ class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitButton onPress={this.submit} />
       </View>
     )
   }
 }
 
 export default AddEntry
+
+function SubmitButton({ onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
--- utils/helpers.js
+++ utils/helpers.js
@@ -78,3 +78,9 @@ export function getMetricMetaInfo(metric) {
 
   return typeof metric === 'undefined' ? info : info[metric]
 }
+
+export function timeToString (time = Date.now()) {
+  const date = new Date(time)
+  const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
+  return todayUTC.toISOString().split('T')[0]
+}
