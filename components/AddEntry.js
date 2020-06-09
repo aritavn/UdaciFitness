6f9066999e612e804e2ae6f5f8764bc9afff6a29
aritavn <anaritavn@gmail.com>
2020-06-09 20:39:16 +0100

    Implement AsyncStorage to persist data

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
 
 class AddEntry extends Component {
 
@@ -55,10 +56,14 @@ class AddEntry extends Component {
       sleep: 0,
       eat: 0
     }))
+
+    submitEntry({ key, entry })
   }
 
   reset = () => {
     const key = timeToString()
+
+    removeEntry(key)
   }
 
   render() {
