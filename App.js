886d2da1b41806f29f83b87302111f69e90a3a03
aritavn <anaritavn@gmail.com>
2020-06-14 16:23:46 +0100

    Add calendar and history

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import History from './components/History'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
@@ -9,7 +10,8 @@ export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
-        <AddEntry />
+        <View style={{height: 20}} />
+        <History />
       </View>
     </Provider>
   )
