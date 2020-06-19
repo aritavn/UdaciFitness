2302e31374ddfe1208393525183e6d29d3b4c299
aritavn <anaritavn@gmail.com>
2020-06-19 19:06:26 +0100

    Add animation

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
 import * as Permissions from 'expo-permissions'
@@ -11,7 +11,8 @@ class Live extends Component {
   state = {
     coords: null,
     status: null,
-    direction: ''
+    direction: '',
+    bounceValue: new Animated.Value(1)
   }
 
   componentDidMount () {
@@ -36,6 +37,15 @@ class Live extends Component {
       distanceInterval: 1
     }, ({ coords }) => {
       const newDirection = calculateDirection(coords.heading)
+      const { direction, bounceValue } = this.state
+
+      if (newDirection !== direction) {
+        Animated.sequence([
+          Animated.timing(bounceValue, { duration: 200, toValue: 1.04 }),
+          Animated.spring(bounceValue, { toValue: 1, friction: 4 })
+        ]).start()
+      }
+
       this.setState(() => ({
         coords,
         status: 'granted',
@@ -45,7 +55,7 @@ class Live extends Component {
   }
 
   render() {
-    const { status, coords, direction } = this.state
+    const { status, coords, direction, bounceValue } = this.state
 
     if (status === null) {
       return <ActivityIndicator style={{marginTop: 30}} />
@@ -82,9 +92,9 @@ class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>
+          <Animated.Text style={[styles.direction, {transform: [{scale: bounceValue}]}]}>
             {direction}
-          </Text>
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
